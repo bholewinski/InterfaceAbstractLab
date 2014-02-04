d62a6863a1161747dea111eead185c71ff517849
bholewinski <bholewinski@L11709038603.wctc.msft>
2014-02-03 20:36:21 -0600

    Second Commit

--- src/lab1/AdvancedJavaCourse.java
+++ src/lab1/AdvancedJavaCourse.java
@@ -8,10 +8,7 @@ import javax.swing.JOptionPane;
  * @author      your name goes here
  * @version     1.00
  */
-public class AdvancedJavaCourse {
-    String courseName;
-    private String courseNumber;
-    private double credits;
+public class AdvancedJavaCourse extends CollegeCourse {
     private String prerequisites;
 
     public AdvancedJavaCourse(String courseName, String courseNumber) {
@@ -22,7 +19,6 @@ public class AdvancedJavaCourse {
     public String getCapitalizedCourseName() {
         return this.getCourseName().toUpperCase();
     }
-
 
     public String getPrerequisites() {
         return prerequisites;
@@ -35,31 +31,5 @@ public class AdvancedJavaCourse {
             System.exit(0);
         }
         this.prerequisites = prerequisites;
-    }
-
-    public void setCredits(double credits) {
-        if(credits < 0.5 || credits > 4.0) {
-            JOptionPane.showMessageDialog(null,
-                    "Error: credits must be in the range 0.5 to 4.0");
-            System.exit(0);
-        }
-        this.setCredits(credits);
-    }
-
-    public String getCourseName() {
-        return courseName;
-    }
-
-    public void setCourseName(String courseName) {
-        this.courseName = courseName;
-    }
-
-    public String getCourseNumber() {
-        return courseNumber;
-    }
-
-    public void setCourseNumber(String courseNumber) {
-        this.courseNumber = courseNumber;
-    }
-    
+    }    
 }
